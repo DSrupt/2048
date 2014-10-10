5b6e1c5bac6bae9a680a9ba8e02002d7fa14a989
Akhil <akhilthampy@yahoo.com>
2014-10-10 11:53:08 -0500

    Rewriting from scratch.

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,17 @@
+import java.awt.*;
+
 /**
  * Created by akhilthampy on 10/10/14.
  */
 public class Game {
+    private int[][] board;
+
+    public void paint(Graphics g) {
+    }
+
+    public void add_tile() {
+    }
+
+    public void merge(int x1, int y1, int x2, int y2, char dir) {
+    }
 }
